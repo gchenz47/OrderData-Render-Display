cbcea0f90f6027dbdf203681260e4b474604f07a
Gavin Chen <cz1989117@gmail.com>
2018-07-17 18:37:12 -0400

    Update OrderbyYear.js

--- components/OrderbyYear.js
+++ components/OrderbyYear.js
@@ -47,7 +47,7 @@ class OrderbyYear extends Component {
     {
       title: 'Date',
       dataIndex: 'created_at',
-      width: 100
+      width: 130
     },
     {
       title: 'Price',
@@ -70,7 +70,7 @@ class OrderbyYear extends Component {
                   {data[key].length} orders created in {key}
                 </Text>
                   {this.state.showYearOrders ?
-                    <Table height={40*(data[key].length+1)} columnWidth={80} columns={columns} dataSource={data[key]} />
+                    <Table height={40*(data[key].length+1)} columnWidth={63} columns={columns} dataSource={data[key]} />
                     : null}
               </ScrollView>
             );
